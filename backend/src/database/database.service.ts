c4444ae44cb2ed8910de6c73a3cddf93dab01065
Nelson Joel Velez Cabrera <nvelez4967@utm.edu.ec>
2025-07-26 00:23:03 -0500

    conexion a la db 3

--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -14,7 +14,7 @@ export class DatabaseService {
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
             database: process.env.DB_NAME,
-            ssl: process.env.DB_SSL === 'true',
+            ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
         })
     };
 
