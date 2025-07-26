9d17d72d62963984879c1f81ebd0ea7ddaee6d51
Nelson Joel Velez Cabrera <nvelez4967@utm.edu.ec>
2025-07-25 23:05:38 -0500

    crear usuario a publico

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { privateGuard, publicGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
     { path: 'auth', canActivate: [publicGuard], loadChildren: () => import('./auth/auth.routes') },
-    { path: 'users', canActivate: [privateGuard], loadComponent: () => import('../app/pages/users/users') },
+    { path: 'users', canActivate: [publicGuard], loadComponent: () => import('../app/pages/users/users') },
     { path: 'home', canActivate: [privateGuard], loadComponent: () => import('./pages/home/home') },
     { path: 'donors', canActivate: [privateGuard], loadComponent: () => import('./pages/donors/donors') },
     { path: 'donations', canActivate: [privateGuard], loadComponent: () => import('./pages/donations/donations') },
