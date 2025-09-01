7bc1bc80c82452454187dae087f80a74b17d847d
Nelson Joel Cabrera <ensondev@gmail.com>
2025-09-01 00:51:22 -0500

    Update database.service.ts

--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -12,7 +12,7 @@ export class DatabaseService {
             host: process.env.DB_HOST,
             port: Number(process.env.DB_PORT),
             user: process.env.DB_USER,
-            password: process.env.DB_PASS,
+            password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
             /* ssl: { rejectUnauthorized: false }, */
         })
