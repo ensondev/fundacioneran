564c58711a684027ec2e06aaadb7975b424422ba
Nelson Joel Velez Cabrera <nvelez4967@utm.edu.ec>
2025-09-02 23:07:42 -0500

    enviroments cambiadas a vercel

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: "https://fundacioneran.onrender.com"
+    /* API_URL: "https://fundacioneran.onrender.com" */
+    API_URL: "https://fundacioneran.vercel.app/"
 };
